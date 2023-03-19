06649603d3cd4b8d6e38f296b0afeecdaf10a5b7
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-19 16:39:26 -0600

    Fix aliases variable name for special page

--- EmailPage.alias.php
+++ EmailPage.alias.php
@@ -6,74 +6,74 @@
  * @ingroup Extensions
  */
 
-$aliases = [];
+$specialPageAliases = [];
 
 /** English
  * @author Jon Harald Søby
  */
-$aliases['en'] = [
+$specialPageAliases['en'] = [
 	'EmailPage' => [ 'EmailPage' ],
 ];
 
 /** Arabic (العربية]
  * @author Meno25
  */
-$aliases['ar'] = [
+$specialPageAliases['ar'] = [
 	'EmailPage' => [ 'مراسلة_الصفحة' ],
 ];
 
 /** Egyptian Spoken Arabic (مصرى]
  * @author Meno25
  */
-$aliases['arz'] = [
+$specialPageAliases['arz'] = [
 	'EmailPage' => [ 'مراسلة_الصفحة' ],
 ];
 
 /** French (Français] */
-$aliases['fr'] = [
+$specialPageAliases['fr'] = [
 	'EmailPage' => [ 'EnvoyerPage', 'Envoyer Page' ],
 ];
 
 /** Galician (Galego] */
-$aliases['gl'] = [
+$specialPageAliases['gl'] = [
 	'EmailPage' => [ 'Enviar páxinas por correo electrónico' ],
 ];
 
 /** Hebrew (עברית]
  * @author Rotem Liss
  */
-$aliases['he'] = [
+$specialPageAliases['he'] = [
 	'EmailPage' => [ 'שליחת_דף_בדואר' ],
 ];
 
 /** Haitian (Kreyòl ayisyen] */
-$aliases['ht'] = [
+$specialPageAliases['ht'] = [
 	'EmailPage' => [ 'EmèlAtik' ],
 ];
 
 /** Hungarian (Magyar] */
-$aliases['hu'] = [
+$specialPageAliases['hu'] = [
 	'EmailPage' => [ 'Szócikk elküldése e-mailben' ],
 ];
 
 /** Luxembourgish (Lëtzebuergesch] */
-$aliases['lb'] = [
+$specialPageAliases['lb'] = [
 	'EmailPage' => [ 'E-Mail-Säiten' ],
 ];
 
 /** Dutch (Nederlands] */
-$aliases['nl'] = [
+$specialPageAliases['nl'] = [
 	'EmailPage' => [ 'PaginaE-mailen', 'PaginaEmailen' ],
 ];
 
 /** Norwegian (bokmål]‬ (‪Norsk (bokmål]‬]
  * @author Jon Harald Søby
  */
-$aliases['no'] = [
+$specialPageAliases['no'] = [
 	'EmailPage' => [ 'Send side som e-post' ],
 ];
 
 /** Pashto (پښتو] */
-$aliases['ps'] = [
+$specialPageAliases['ps'] = [
 	'EmailPage' => [ 'د برېښليک مخونه' ],
 ];
